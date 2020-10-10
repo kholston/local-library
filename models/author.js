cf9c2091f53802f97eaa34321bf894a15365a05c
Ken Holston <git.kholston@gmail.com>
2020-10-09 23:25:24 -0400

    add model schemas and basic routes

---
+++ models/author.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+//Virtual for author's full name
+AuthorSchema.virtual('name').get(() => {
+  let fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name;
+  }
+  if (this.first_name || this.family_name) {
+    fullname = '';
+  }
+  return fullname;
+});
+
+AuthorSchema.virtual('lifespan').get(() => {
+  return (
+    this.date_of_death.getYear() - this.date_of_birth.getYear()
+  ).toString();
+});
+
+AuthorSchema.virtual('url').get(() => {
+  return '/catalog/author/' + this.id;
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
