a7929081c8f9dacdea34c5d8d137d29fa700eecb
Ken Holston <git.kholston@gmail.com>
2020-10-11 22:43:26 -0400

    implement genre_detail function

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book');
+
+var async = require('async');
 
 // Display list of genres
 exports.genre_list = (req, res, next) => {
@@ -17,8 +20,35 @@ exports.genre_list = (req, res, next) => {
 };
 
 // Display detail page for a specific genre
-exports.genre_detail = (req, res) => {
-  res.send('Not Implemented: Genre detail: ' + req.params.id);
+exports.genre_detail = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        var err = new Error('Genre not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render('genre_detail', {
+        title: 'Genre Detail',
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Display Genre create form on Get
