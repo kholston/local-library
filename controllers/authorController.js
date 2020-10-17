9992786f4d989f3906a8c42e093fbd0a7d00c36f
Ken Holston <git.kholston@gmail.com>
2020-10-17 19:58:43 -0400

    implement author_update functions

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -135,7 +135,7 @@ exports.author_delete_get = (req, res, next) => {
 };
 
 // Handle Author delete on POST
-exports.author_delete_post = (req, res, next) => {
+exports.author_delete_post = function (req, res, next) {
   async.parallel(
     {
       author: function (callback) {
@@ -169,11 +169,49 @@ exports.author_delete_post = (req, res, next) => {
 };
 
 // Display Author update form on GET
-exports.author_update_get = (req, res) => {
-  res.send('Not Implemented: Author update GET');
+exports.author_update_get = function (req, res, next) {
+  Author.findById(req.params.id,function(err,author){
+    if(err){return next(err)}
+    if(author == null){
+      var err= new Error('Author not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.render('author_form',{title: 'Update Author',author:author})
+  })
 };
 
 // Handle Author update on POST
-exports.author_update_post = (req, res) => {
-  res.send('Not Implemented: Author update POST');
-};
+exports.author_update_post = [
+  body('first_name').trim().isLength().escape().withMessage('First name must be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+  body('family_name').trim().isLength().escape().withMessage('Family name must be specified.')
+    .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth').optional({checkFalsy:true}).isISO8601().toDate(),
+  body('date_of_death', 'Invalid date of death').optional({checkFalsy:true}).isISO8601().toDate(),
+
+  (req,res,next)=>{
+    const errors = validationResult(req)
+
+    var author = new Author(
+      {
+        first_name:req.body.first_name,
+        family_name:req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death:req.body.date_of_death,
+        _id:req.params.id
+      }
+    );
+
+    if(!errors.isEmpty()){
+      res.render('author_form',{title:"Update Author",author:author,error:errors.array()})
+      return;
+    }
+    else {
+      Author.findByIdAndUpdate(req.params.id,author,{},function(err,theauthor){
+        if(err){return next(err)}
+        res.redirect(theauthor.url);
+      })
+    }
+  }
+];
